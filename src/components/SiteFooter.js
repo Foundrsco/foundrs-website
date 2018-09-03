606806a5ad3b5809ae1ce9d14df90cf9a0f2f9d6
Stef Lewandowski <stef@stef.io>
2018-09-03 11:46:07 +0100

    site footer

---
+++ src/components/SiteFooter.js
@@ -0,0 +1,11 @@
+import Footer from 'react-bulma-components/lib/components/footer'
+
+export default class SiteFooter extends React.Component {
+  render () {
+    return (
+      <Footer>
+        <strong>&copy; Foundrs.co</strong> | Foundrs is a not for profit community organisation. Made with ❤ in London.
+      </Footer>
+    )
+  }
+}
