5391c9c03bb64878db8a9ad11a05817c6f577f31
Stef Lewandowski <stef@stef.io>
2018-09-04 08:33:46 +0100

    ►

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,38 +38,50 @@ module.exports = {
       resolve: `gatsby-plugin-sitemap`
     },
     {
-      resolve: `gatsby-plugin-favicon`,
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        logo: './src/img/foundrs-icon.png',
-
-        // WebApp Manifest Configuration
-        appName: 'Foundrs',
-        appDescription: `A community of the world's best founders`,
-        developerName: 'Stef Lewandowski',
-        developerURL: 'https://stef.io',
-        dir: 'auto',
-        lang: 'en-US',
-        background: '#0a0a0a',
-        theme_color: '#fff',
-        display: 'standalone',
-        orientation: 'any',
-        start_url: '/?homescreen=1',
-        version: '1.0',
-
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: false,
-          favicons: true,
-          firefox: true,
-          opengraph: false,
-          twitter: false,
-          yandex: false,
-          windows: false
-        }
+        name: `Foundrs`,
+        short_name: `Foundrs`,
+        start_url: `/?homescreen=1`,
+        background_color: `#0a0a0a`,
+        theme_color: `#ffffff`,
+        display: `minimal-ui`,
+        icon: `src/img/foundrs-icon.png` // This path is relative to the root of the site.
       }
     },
+    // {
+    //   resolve: `gatsby-plugin-favicon`,
+    //   options: {
+    //     logo: './src/img/foundrs-icon.png',
+
+    //     // WebApp Manifest Configuration
+    //     appName: 'Foundrs',
+    //     appDescription: `A community of the world's best founders`,
+    //     developerName: 'Stef Lewandowski',
+    //     developerURL: 'https://stef.io',
+    //     dir: 'auto',
+    //     lang: 'en-US',
+    //     background: '#0a0a0a',
+    //     theme_color: '#fff',
+    //     display: 'standalone',
+    //     orientation: 'any',
+    //     start_url: '/?homescreen=1',
+    //     version: '1.0',
+
+    //     icons: {
+    //       android: true,
+    //       appleIcon: true,
+    //       appleStartup: true,
+    //       coast: false,
+    //       favicons: true,
+    //       firefox: true,
+    //       opengraph: false,
+    //       twitter: false,
+    //       yandex: false,
+    //       windows: false
+    //     }
+    //   }
+    // },
     'gatsby-plugin-page-transitions',
     'gatsby-plugin-netlify' // make sure to keep it last in the array
   ]
